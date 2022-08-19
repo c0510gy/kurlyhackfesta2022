2a764a2f304cc90dfc70d216f808dee3c17c1011
kahye-lee <kahyelee.g@gmail.com>
2022-08-19 18:42:56 +0900

    applied basic eslint and prettier

--- client/web-v1/src/app.tsx
+++ client/web-v1/src/app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const App = function appComponent(): JSX.Element {
   return <div>gravimetric</div>;
--- client/web-v1/src/index.tsx
+++ client/web-v1/src/index.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./app";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
 root.render(<App />);
