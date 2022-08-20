4cc43e9ffbc89cf63e258b704c1dd02d952b7d38
kahye-lee <kahyelee.g@gmail.com>
2022-08-20 15:02:54 +0900

    applied eslint and prettier

--- client/web-v1/webpack.config.js
+++ client/web-v1/webpack.config.js
@@ -1,45 +1,112 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDevMode = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  mode: "development",
-  entry: ["./src/index.tsx"],
-  devtool: "source-map",
+  mode: 'development',
+  entry: ['./src/index.tsx'],
+  devtool: 'source-map',
   module: {
     rules: [
       {
-        test: /\.?js$/,
+        test: /\.?jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },
-      { test: /\.tsx?$/, exclude: /node_modules/, loader: "ts-loader" },
+      { test: /\.tsx?$/, exclude: /node_modules/, loader: 'ts-loader' },
+      // scss all except modules
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        use: [
+          isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                mode: 'icss',
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      // scss modules
+      {
+        test: /\.module.s[ac]ss$/i,
+        use: [
+          // Fallback to style-loader in development
+          isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                exportLocalsConvention: 'camelCase',
+              },
+              sourceMap: isDevMode,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                config: path.resolve(__dirname, 'postcss.config.js'),
+              },
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer `dart-sass`
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      // font
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
-  resolve: { extensions: [".tsx", ".ts", ".js"] },
+  resolve: { extensions: ['.tsx', '.ts', '.js', '.scss'] },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     clean: true,
   },
   devServer: {
     historyApiFallback: true,
-    static: "./dist",
+    static: './dist',
     port: 3000,
     hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevMode ? '[name].css' : '[name].[contenthash].css',
+      chunkFilename: isDevMode ? '[id].css' : '[id].[contenthash].css',
     }),
   ],
 };
