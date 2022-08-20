0b1855f085e6938a799f700dd08c8ca3c89411d9
kkoma2623 <jiwook.dev@gmail.com>
2022-08-20 17:47:56 +0900

    cognito end

--- client/web-v1/src/app.tsx
+++ client/web-v1/src/app.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layouts from './layout';
 import Landing from './pages/Landing';
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import awsconfig from "./awsconfig.js";
+
+Amplify.configure(awsconfig);
 
 const App = (): JSX.Element => {
   return (
     <Routes>
-      <Route path="/" element={<Layouts />}>
-        <Route index element={<Landing />} />
-      </Route>
+        <Route path="/" element={<Layouts />}>
+          <Route index element={<Landing />} />
+        </Route>
     </Routes>
   );
 };
 
-export default App;
+export default withAuthenticator(App);
--- client/web-v1/src/layout/index.tsx
+++ client/web-v1/src/layout/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import {Auth} from "aws-amplify";
+
+async function signOut() {
+  try {
+      await Auth.signOut();
+  } catch (error) {
+      console.log('error signing out: ', error);
+  }
+}
 
 const Layouts = () => {
   return (
     <>
+      <button onClick={signOut}>Sign out!!!</button>
       <Outlet />
     </>
   );
