1753da31d145c8ddaab127258f141162cbaa38bc
kkoma2623 <jiwook.dev@gmail.com>
2022-08-22 01:42:58 +0900

    can test random create log

--- client/web-v1/src/pages/Picking/BasketView/index.tsx
+++ client/web-v1/src/pages/Picking/BasketView/index.tsx
@@ -8,15 +8,19 @@ for(let i=0; i<1000; ++i) {
   pickingEvents[i] = {};
 }
 
-pickingEvents[1] = { worker_id: 0, busket_id: 1, product_id: 9, weight: 0.41740172538650305, operation: 'PUT', label: 'False', predict: 'True'};
-pickingEvents[0] = { worker_id: 1, busket_id: 0, product_id: 2, weight: 0.4940190375572947, operation: 'PUT', label: 'False', predict: 'True'};
-pickingEvents[1] = { worker_id: 2, busket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
-pickingEvents[2] = { worker_id: 2, busket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
-pickingEvents[3] = { worker_id: 2, busket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
-pickingEvents[4] = { worker_id: 2, busket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
-pickingEvents[5] = { worker_id: 2, busket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
-pickingEvents[6] = { worker_id: 2, busket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
-pickingEvents[102] = { worker_id: 2, busket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
+pickingEvents[1] = { worker_id: 0, basket_id: 1, product_id: 9, weight: 0.41740172538650305, operation: 'PUT', label: 'False', predict: 'True'};
+pickingEvents[0] = { worker_id: 1, basket_id: 0, product_id: 2, weight: 0.4940190375572947, operation: 'PUT', label: 'False', predict: 'True'};
+pickingEvents[1] = { worker_id: 2, basket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
+pickingEvents[2] = { worker_id: 2, basket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
+pickingEvents[3] = { worker_id: 2, basket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
+pickingEvents[4] = { worker_id: 2, basket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
+pickingEvents[5] = { worker_id: 2, basket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
+pickingEvents[6] = { worker_id: 2, basket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
+pickingEvents[102] = { worker_id: 2, basket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
+
+const getRandomInt = (maxNum: number, minNum: number) => {
+  return Math.floor(Math.random() * (maxNum-minNum)) + minNum;
+}
 
 const BasketView = () => {
   const [pickingList, setPickingList] = useState([]);
@@ -43,30 +47,46 @@ const BasketView = () => {
     }
   }
 
+  const countErrorBaskets = (errorBasketSumOrigin: number, rowIdx: number, colIdx: number): number => {
+    const basketIndex = [0, 0, 0, 0, 0];
+    for (let i=0; i<5; ++i){
+      basketIndex[i] = (rowIdx*100 + colIdx*5 + i);
+    }
+    let errorBasketSum = 0;
+    for(let i=0; i<5; ++i) {
+      if (pickingEvents[basketIndex[i]]?.predict === 'True') {
+        ++errorBasketSum;
+      }
+    }
+
+    if (errorBasketSum !== errorBasketSumOrigin) {
+      changeHandler(rowIdx, colIdx, errorBasketSum);
+    }
+
+    return errorBasketSum;
+  }
+
+  // temp function for test
+  const changePickingEventHandler = () => {
+    const basketIndex = getRandomInt(1000, 0);
+    const workerId = getRandomInt(5, 0);
+    const productId = getRandomInt(10, 0);
+    const predict = getRandomInt(2, 0) === 1 ? "True" : "False";
+    pickingEvents[basketIndex] = {worker_id: workerId, basket_id: basketIndex, product_id: productId, predict: predict}
+    const [row, col] = [Math.floor(basketIndex/100), Math.floor(Math.floor(basketIndex%100)/5)];
+    countErrorBaskets(pickingList[row][col], row, col);
+  }
+
   return (
     <div className={styles.basketView}>
+      <button onClick={changePickingEventHandler}>random create</button>
       <table>
         <tbody className={styles.tableBody}>
           {pickingList.map((row: any, rowIdx: number) => {
             return (
               <tr key={rowIdx}>
                 {row.map((value: number, colIdx: number) => {
-                    const basketIndex = [0, 0, 0, 0, 0];
-                    for (let i=0; i<5; ++i){
-                      basketIndex[i] = (rowIdx*100 + colIdx*5 + i);
-                    }
-                    const errorBasketSumOrigin = value;
-
-                    let errorBasketSum = 0;
-                    for(let i=0; i<5; ++i) {
-                      if (pickingEvents[basketIndex[i]]?.predict === 'True') {
-                        ++errorBasketSum;
-                      }
-                    }
-
-                    if (errorBasketSum !== errorBasketSumOrigin) {
-                      changeHandler(rowIdx, colIdx, errorBasketSum);
-                    }
+                  const errorBasketSum =  countErrorBaskets(value, rowIdx, colIdx);
                     return <td
                             key={colIdx}
                             className={rowIdx%2 ? styles.tdEnter : styles.tdNoEnter}
