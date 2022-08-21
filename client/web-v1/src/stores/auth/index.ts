011e0bc80fe7f70da89f6dbbf7c3286e5f20eb52
kahye-lee <kahyelee.g@gmail.com>
2022-08-21 20:26:20 +0900

    move sign out logic to authstore by making action

--- client/web-v1/src/stores/auth/index.ts
+++ client/web-v1/src/stores/auth/index.ts
@@ -1,34 +1,37 @@
-import { makeAutoObservable } from 'mobx'
-import { Auth } from 'aws-amplify'
+import { makeAutoObservable } from 'mobx';
+import { Auth } from 'aws-amplify';
 
 const authStore = function createAuthStore() {
-    return makeAutoObservable({
-        idToken: "",
-        accessToken: "",
-        name: "",
-        updateToken(idToken?: string, accessToken?: string, name?: string):void {
-            this.idToken = idToken || this.idToken;
-            this.accessToken = accessToken || this.accessToken;
-            this.name = name || this.name;
-        },
-        removeToken(): void {
-            this.idToken = '';
-            this.accessToken = '';
-            this.name = '';
-        },
-        getIdToken(): void {
-            Auth.currentAuthenticatedUser().then((user) => {
-                const idToken = user.signInUserSession.idToken.jwtToken;
-                if (idToken === this.idToken) 
-                    return idToken
-                const accessToken = user.signInUserSession.accessToken.jwtToken;
-                const name = user.attributes.nickname;
+  return makeAutoObservable({
+    idToken: '',
+    accessToken: '',
+    name: '',
+    updateToken(idToken?: string, accessToken?: string, name?: string): void {
+      this.idToken = idToken || this.idToken;
+      this.accessToken = accessToken || this.accessToken;
+      this.name = name || this.name;
+    },
+    removeToken(): void {
+      this.idToken = '';
+      this.accessToken = '';
+      this.name = '';
+    },
+    getIdToken(): void {
+      Auth.currentAuthenticatedUser().then((user) => {
+        const idToken = user.signInUserSession.idToken.jwtToken;
+        if (idToken === this.idToken) return idToken;
+        const accessToken = user.signInUserSession.accessToken.jwtToken;
+        const name = user.attributes.nickname;
 
-                this.updateToken(idToken, accessToken, name);
-                return idToken;
-            })
-        }
-    });
+        this.updateToken(idToken, accessToken, name);
+        return idToken;
+      });
+    },
+    async signOut(): Promise<void> {
+      await Auth.signOut();
+      this.removeToken();
+    },
+  });
 };
 
 export default authStore;
