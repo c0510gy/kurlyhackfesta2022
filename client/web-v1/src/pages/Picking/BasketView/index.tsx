cd475c2cc924180285a6093cab8469394f205d5c
kkoma2623 <jiwook.dev@gmail.com>
2022-08-21 21:45:23 +0900

    can see basketView with color

--- client/web-v1/src/pages/Picking/BasketView/index.tsx
+++ client/web-v1/src/pages/Picking/BasketView/index.tsx
@@ -9,15 +9,20 @@ for(let i=0; i<1000; ++i) {
 }
 
 pickingEvents[1] = { worker_id: 0, busket_id: 1, product_id: 9, weight: 0.41740172538650305, operation: 'PUT', label: 'False', predict: 'True'};
-pickingEvents[0] = { worker_id: 1, busket_id: 0, product_id: 2, weight: 0.4940190375572947, operation: 'PUT', label: 'False', predict: 'False'};
+pickingEvents[0] = { worker_id: 1, busket_id: 0, product_id: 2, weight: 0.4940190375572947, operation: 'PUT', label: 'False', predict: 'True'};
 pickingEvents[1] = { worker_id: 2, busket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
+pickingEvents[2] = { worker_id: 2, busket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
+pickingEvents[3] = { worker_id: 2, busket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
+pickingEvents[4] = { worker_id: 2, busket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
+pickingEvents[5] = { worker_id: 2, busket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
+pickingEvents[6] = { worker_id: 2, busket_id: 1, product_id: 9, weight: 0.4969732191272813, operation: 'PUT', label: 'False', predict: 'True'};
 
 const BasketView = () => {
   const [pickingList, setPickingList] = useState([]);
   useEffect(() => {
     const tempArr = [];
-    for(let row=0; row<25; ++row){
-      tempArr.push(new Array(40).fill(0));
+    for(let row=0; row<10; ++row){
+      tempArr.push(new Array(20).fill(0));
     }
     setPickingList(tempArr);
   }, []);
@@ -26,33 +31,35 @@ const BasketView = () => {
     const copy = [...pickingList];
     copy[rowIdx][colIdx] = value;
     setPickingList(copy);
-    console.log('called')
   }
 
   return (
     <div className={styles.basketView}>
       <table>
-        <tbody>
+        <tbody className={styles.tableBody}>
           {pickingList.map((row: any, rowIdx: number) => {
             return (
-              <tr key={rowIdx} style={rowIdx%2 ? {marginBottom: 500} : {}}>
-                {row.map((value: any, colIdx: number) => {
-                  const basketIndex = [0, 0, 0, 0, 0];
-                  for (let i=0; i<5; ++i){
-                    basketIndex[i] = (rowIdx*100 + colIdx*5 + i);
-                  }
-                  const errorBasketSumOrigin = pickingList[rowIdx][colIdx];
-                  let errorBasketSum = 0;
-                  for(let i=0; i<5; ++i) {
-                    if (pickingEvents[basketIndex[i]]?.predict === 'True') {
-                      ++errorBasketSum;
+              <tr key={rowIdx}>
+                {row.map((value: number, colIdx: number) => {
+                    const basketIndex = [0, 0, 0, 0, 0];
+                    for (let i=0; i<5; ++i){
+                      basketIndex[i] = (rowIdx*100 + colIdx*5 + i);
                     }
-                  }
-                  if (errorBasketSum !== errorBasketSumOrigin) {
-                    handleChange(rowIdx, colIdx, errorBasketSum);
-                  }
-                  return <td key={colIdx}>{pickingList[rowIdx][colIdx]}</td>
-                })}
+                    const errorBasketSumOrigin = value;
+
+                    let errorBasketSum = 0;
+                    for(let i=0; i<5; ++i) {
+                      if (pickingEvents[basketIndex[i]]?.predict === 'True') {
+                        ++errorBasketSum;
+                      }
+                    }
+
+                    if (errorBasketSum !== errorBasketSumOrigin) {
+                      handleChange(rowIdx, colIdx, errorBasketSum);
+                    }
+                    return <td key={colIdx} className={rowIdx%2 ? styles.tdEnter : styles.tdNoEnter}
+                            style={errorBasketSum ? {"color": "red", "backgroundColor": "pink"} : {"backgroundColor" : "green"}}>{pickingList[rowIdx][colIdx]}</td>
+                  })}
               </tr>
             )
           })}
